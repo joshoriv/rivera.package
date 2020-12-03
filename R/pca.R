0c0dabaffb253d07628b777ce534a3f57b1eec56
joshoriv <joshua.rivera@selu.edu>
2020-12-03 09:53:14 -0600

    more updates

--- R/pca.R
+++ R/pca.R
@@ -1,6 +1,7 @@
 #' Performs and visualizes a Principal Components Analysis for shape data
 #' 
 #' @param gpa General Procrustes Analysis for shape data
+#' @param pca Principal Components Analysis for shape data
 #' @importFrom geomorph gm.prcomp
 #' @return PCA graph for shape data (graph)
 #' 
